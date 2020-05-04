962da7867386fd66945a6f367e4be7ae21ffa6d0
Danny <dan.pazuchowski@gmail.com>
2020-05-03 19:35:37 -0700

    fetch all url

---
+++ chp1/fetch-urls-all.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	url := os.Args[1]
+	urls := []string{url, url, url, url, url}
+	for _, u := range urls {
+		go fetch(u, ch)
+	}
+	for range urls {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	fmt.Println(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("While reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
